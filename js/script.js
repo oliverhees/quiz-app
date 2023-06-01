87757b19c7715a4f0e8ff2ac71003eb0f2c43d78
Oliver Hees <oliverhees@gmail.com>
2023-06-01 09:27:04 +0200

    edit darkmode

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
+// Dark Mode
 const mainElement = document.querySelector("body");
 const darkModeToggle = document.querySelector(".dark-mode");
 // console.log(mainElement);
 
 darkModeToggle.addEventListener("click", () => {
   mainElement.classList.toggle("dark");
-  console.log("darkmode");
+  //console.log("darkmode");
 });
